852a04d6740e20a79869a40ef4808beb7e6b437b
pranali2808 <ranepranali1997@gmail.com>
2022-04-19 13:35:44 +0530

    given null mood should return happy

---
+++ MoodAnalyserTestCase/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MoodAnalyserTestCase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoodAnalyserTestCase")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b571305-a3a5-44b2-8beb-c72f3705fded")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MoodAnalyserTestCase/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyser;
+
+namespace MoodAnalyserTestCase
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SadReturn()
+        {
+            string expected = "sad";//arrange
+            MoodCheck moodAnalyser = new MoodCheck("sad");//act
+            Assert.AreEqual(expected, moodAnalyser.AnalyzeMood());
+        }
+
+        [TestMethod]
+        public void HappyReturn()
+        {
+            //Arrange
+            string expected = "happy";
+            MoodCheck moodAnalyser = new MoodCheck("happy");//act
+            Assert.AreEqual(expected, moodAnalyser.AnalyzeMood());
+        }
+
+        public void ImInSadMoodSAD()
+        {
+            //Arrange
+            string message = "I am in sad mood.";
+            MoodCheck moodAnalyser = new MoodCheck(message);
+            string mymood = moodAnalyser.AnalyzeMood();//act
+            Assert.AreEqual("sad", mymood);
+        }
+
+        [TestMethod]
+        public void ImInAnyMoodHAPPY()
+        {
+            //Arrange
+            string message = "I am in any mood.";
+            MoodCheck moodAnalyser = new MoodCheck(message);
+            string mymood = moodAnalyser.AnalyzeMood();//act
+            Assert.AreEqual("happy", mymood);
+        }
+        
+        [TestMethod]
+        public void NullMoodHappy()
+        {
+            //Arrange
+            string expected = "happy";
+            MoodCheck moodAnalyser = new MoodCheck(null);//act
+            Assert.AreEqual(expected, moodAnalyser.AnalyzeMood());
+        }
+    }
+}
+
+
+
